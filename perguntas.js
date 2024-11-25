281af0342735579aca1ad58e851e13fa20e8a698
Nicolaskuririn <nicolassilvateixeira6@gmail.com>
2024-11-25 08:15:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'nba',
+    'quantos titulos o lebron tem ?',
+    'sÄo 4'
+)
+
+criaCartao(
+    'nba',
+    'Qual e codenome do kevin durant?',
+    'e easymoneysniper'
+)
+
+criaCartao(
+    'nba',
+    'quem e o mellhor jogador do basquete o la goat?',
+    'e o michael jordan'
+)
+
+criaCartao(
+    'nba',
+    'quem e o novato mais novo a ganhar o mvp da temprada',
+    'e o derrick rose'
+)
